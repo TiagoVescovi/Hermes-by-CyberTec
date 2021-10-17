98b5bc1ffab0733fdebd4c94bc81bf4a27a3fd27
Tiago Véscovi <vescovitiago@gmail.com>
2021-10-16 23:24:19 -0300

    Versión definitiva de BD con todos los cambios necesarios

--- crearBD.sql
+++ crearBD.sql
@@ -31,12 +31,6 @@ idMateria INT(5) NOT NULL auto_increment,
 nombre VARCHAR(30) NOT NULL,
 PRIMARY KEY(idMateria));
 
-CREATE TABLE Enseña(
-idMateria INT(5) NOT NULL,
-ciProfesor INT(8) NOT NULL,
-FOREIGN KEY(ciProfesor) REFERENCES Docente(cedula),
-PRIMARY KEY(idMateria,ciProfesor));
-
 CREATE TABLE Horario(
 ciProfesor INT(8) NOT NULL,
 horaIni DATETIME NOT NULL,
@@ -52,12 +46,19 @@ PRIMARY KEY(idOrientacion));
 
 CREATE TABLE Grupo(
 idGrupo INT(5) NOT NULL auto_increment,
-nombre VARCHAR(10),
+nombreGr VARCHAR(10),
 año INT(4),
 idOrientacion INT(4),
 FOREIGN KEY(idOrientacion) REFERENCES Orientacion(idOrientacion),
 PRIMARY KEY(idGrupo));
 
+CREATE TABLE Enseña(
+idMateria INT(5) NOT NULL,
+ciProfesor INT(8) NOT NULL,
+idGrupo INT(5) NOT NULL,
+FOREIGN KEY(ciProfesor) REFERENCES Docente(cedula),
+FOREIGN KEY(idGrupo) REFERENCES Grupo(idGrupo),
+PRIMARY KEY(idMateria,ciProfesor));
 
 CREATE TABLE Consulta(
 idConsulta INT(20) NOT NULL auto_increment,
@@ -71,44 +72,39 @@ FOREIGN KEY(idMateria) REFERENCES Materia(idMateria),
 FOREIGN KEY(idGrupo) REFERENCES Grupo(idGrupo),
 PRIMARY KEY(idConsulta));
 
-
 CREATE TABLE Chat(
-idConsulta INT(20) NOT NULL,
+idChat INT(20) NOT NULL,
 horaIni DATETIME,
 horaFin DATETIME,
-FOREIGN KEY(idConsulta) REFERENCES Consulta(idConsulta),
-PRIMARY KEY(idConsulta));
+FOREIGN KEY(idChat) REFERENCES Consulta(idConsulta),
+PRIMARY KEY(idChat));
 
 CREATE TABLE mensaje(
-	idMensaje INT NOT NULL auto_increment,
-	contenido VARCHAR(45) NOT NULL,
-	hora DATETIME,
-	idChat INT(20) NOT NULL,
-	idAutor INT(8) NOT NULL,
-	FOREIGN KEY(idChat) REFERENCES Consulta(idConsulta),
-	FOREIGN KEY(idAutor) REFERENCES Persona(cedula),
-	PRIMARY KEY(idMensaje));
-
+idMensaje INT NOT NULL auto_increment,
+contenido VARCHAR(700) NOT NULL,
+hora DATETIME,
+idChat INT(20) NOT NULL,
+idAutor INT(8) NOT NULL,
+FOREIGN KEY(idChat) REFERENCES Chat(idChat),
+FOREIGN KEY(idAutor) REFERENCES Persona(cedula),
+PRIMARY KEY(idMensaje));
 
 CREATE TABLE Participa(
-idConsulta INT(20) NOT NULL,
+idChat INT(20) NOT NULL,
 ciAlumno INT(8) NOT NULL,
-FOREIGN KEY(idConsulta) REFERENCES Consulta(idConsulta),
+FOREIGN KEY(idChat) REFERENCES Chat(idChat),
 FOREIGN KEY(ciAlumno) REFERENCES Alumno(cedula),
-PRIMARY KEY(idConsulta,ciAlumno));
+PRIMARY KEY(idChat,ciAlumno));
 
 CREATE TABLE Asincronica(
-idConsulta INT(20) NOT NULL,
+idAsincronica INT(20) NOT NULL,
 estado ENUM('Realizada','Contestada','Recibida') NOT NULL,
-contenido BLOB,
-FOREIGN KEY(idConsulta) REFERENCES Consulta(idConsulta),
-PRIMARY KEY(idConsulta));
+consulta VARCHAR(700),
+respuesta VARCHAR(700),
+FOREIGN KEY(idAsincronica) REFERENCES Consulta(idConsulta),
+PRIMARY KEY(idAsincronica));
 
 /* INGRESO DE DATOS DE PRUEBA*/
-
-INSERT INTO GRUPO (Nombre) VALUES ("3°BA");
-INSERT INTO Materia(nombre) VALUES("Matemática");
-INSERT INTO Materia (nombre) VALUES ("Filosofía");
 
 INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(429329192,"Juan","Perez","contra123");
 INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(413515145,"Roberto","Díaz","Password321");
@@ -116,7 +112,35 @@ INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(566434567,"Agustina","F
 INSERT INTO docente SELECT cedula from persona;
 INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(145162774,"Javier","González","javidi102");
 INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(145632623,"Martín","Fernández","estoesunacontraseña");
-INSERT INTO alumno(cedula) values(145632623);
 INSERT INTO Persona(cedula,nombre,apellido,passwd)VALUES(423643512,"Mariana","Acosta","contraseña");
 INSERT INTO Administrador(cedula,cargo)VALUES(423643512,"Otro/a");
+
+INSERT INTO ORIENTACION(nombre) VALUES("Robótica");
+
+INSERT INTO GRUPO (nombreGr,idOrientacion) VALUES ("3°BA",1);
+
+INSERT INTO Materia(nombre) VALUES("Matemática");
+
+INSERT INTO Materia (nombre) VALUES ("Filosofía");
+
+INSERT INTO Enseña (idMateria,ciProfesor,idGrupo) VALUES(1,429329192,1);
+
+INSERT INTO alumno(cedula,idGrupo) values(145632623,1);
+
+/* SELECTS DE TODAS LAS TABLAS*/
+
+SELECT * FROM administrador;
+SELECT * FROM alumno;
+SELECT * FROM asincronica;
+SELECT * FROM chat;
+SELECT * FROM consulta;
+SELECT * FROM docente;
+SELECT * FROM enseña;
+SELECT * FROM grupo;
+SELECT * FROM horario;
+SELECT * FROM materia;
+SELECT * FROM mensaje;
+SELECT * FROM orientacion;
+SELECT * FROM participa;
+SELECT * FROM persona;
 
